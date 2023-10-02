3e24943fe750a90fd4db7407f24aea3118a2f1e9
CarlosCastanedaDev <carlos@nemin.dev>
2023-10-02 11:26:21 -0500

    /users route working

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  get("/", { :controller => "users", :action => "index" })
 
+  # routes for Users
+  get("/users", { :controller => "users", :action => "index" })
 end
